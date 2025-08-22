54e02005ccaa5dd350e85ee0467d77d16062e2d9
CristianRecinos98 <78291865+CristianRecinos98@users.noreply.github.com>
2025-08-21 22:32:32 -0700

    Update testOnPress.lua

--- testOnPress.lua
+++ testOnPress.lua
@@ -34,14 +34,21 @@ end
 
 Image(GetFile("02X561A743D35260BF97698F1G3AB3AE01J9MEE0.png"))
 
+-- BELOW IS 'EDITOR INTERFACE CLASS' -inherits from 'VerticalGroup'?
+-- this will have the utility tab bar, the file tab bar, and space for whatever file editor for most of the screen
+
 tabBar = LayeredGroup({width: "100%", height: "30px"})
 tabBar.BackgroundImage = Image({width: "100%", height: "100%", imageFile: "UUID goes here?"})
 tabBar.HG = HorizontalGroup({width: "100%", height: "100%", spacing: "left-align"})
 
-
+-- maybe also write how this is tokened in the save file. soooo will it be stored/read differently than normal class files?
+-- wait this IS the save file isn't it?
 tab = LayeredGroup({stretchWeight: "1", min-width: "20px", max-width: "100px", height:"100%"})
 tab.BackgroundImage = Image({width: "100%", height: "100%", imageFile: "UUID goes here?"})
 tab.HG = HorizontalGroup({width: "100%", height: "100%", spacing: "custom"})
 tab.HG[0] = Image({width: "100px", height: "auto"}) -- ICON
 tab.HG[1] = Text({width: "auto", height: "auto", text: ""})
+
 tab.XButton = Image({width: "100%", height: "100%", opacity: "0", imageFile: "UUID goes here?"})
+
+
